d5200362e3834738c8f611895cfc54bb847c42a6
Andris Molchanov <83452268+akatastrofa@users.noreply.github.com>
2021-09-16 17:24:56 +0500

    Update exc_classes.py

--- exc_classes.py
+++ exc_classes.py
@@ -10,22 +10,26 @@ class CurrencyConverter:
     def converter(quote: str, base: str, amount: str):
 
         if quote == base:
-            raise ConvertionException(f'Не удалось перевести одинаковые валюты {base}.')
+            raise ConvertionException(f'Не удалось перевести одинаковые валюты {base}.\
+\nНажмите /help, чтобы узнать, как пользоваться ботом')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+            raise ConvertionException(f'Не удалось обработать валюту {quote}\
+\nНажмите /help, чтобы узнать, как пользоваться ботом')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {base}')
+            raise ConvertionException(f'Не удалось обработать валюту {base}\
+\nНажмите /help, чтобы узнать, как пользоваться ботом')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertionException(f'Не удалось обработать количество валюты {amount}')
+            raise ConvertionException(f'Не удалось обработать количество валюты {amount}\
+\nНажмите /help, чтобы узнать, как пользоваться ботом')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
